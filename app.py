d8e15bd9c9ce2a03b9149c9e7ba533e8ea80be5e
Kaushal-Ushur <kaushal.hebbar@ushur.com>
2025-03-05 14:09:31 +0530

    using ushur llm

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ huggingface_hub.login(hf_token)
 
 class InferlessPythonModel:
     def initialize(self):
-        model_name = "google/gemma-2-9b-it"
+        # model_name = "google/gemma-2-9b-it"
+        model_name = "ushuradmin/ushur-llm-best-practices-v1.1"
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.llm = LLM(model=model_name,enforce_eager=True)
+        self.llm = LLM(model=model_name, enforce_eager=True)
         # self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="cuda")
 
     def infer(self, inputs):
         prompt = inputs["prompt"]
         temperature = inputs.get("temperature", 0.7)
         top_p = inputs.get("top_p", 0.1)
-        top_k = int(inputs.get("top_k",40))
+        top_k = int(inputs.get("top_k", 40))
         repetition_penalty = float(inputs.get("repetition_penalty", 1.18))
         max_new_tokens = inputs.get("max_new_tokens", 256)
       
